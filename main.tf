6f347bf43618bb06192426902fec31a0268f6161
thaheershaik92 <128355075+thaheershaik92@users.noreply.github.com>
2024-02-26 17:52:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,31 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
+
+resource "aws_s3_bucket" "terrforn_state" {
+  bucket = "my-tf-test-luci"
+
+  versioning {
+   enabled = true
+   }
+
+   rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-west-2"
-}
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "pay_per_requested"
+  hash_key       = "LockId"
 
+  attribute {
+    name = "LockId"
+    type = "S"
+  }
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
